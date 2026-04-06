e591adf3da4b1958608a87575fc42a4b9d756619
chefbeom13 <chefbeom13@naver.com>
2026-04-06 10:11:27 +0900

    git init set

---
+++ src/main/java/com/example/canchu/CanchuApplication.java
@@ -0,0 +1,13 @@
+package com.example.canchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanchuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanchuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/canchu/CanchuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanchuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
